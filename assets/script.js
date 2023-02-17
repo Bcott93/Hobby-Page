7b221c4832284c2ed63b685069903db67abbcf96
Benjamin <bcott93@gmail.com>
2023-02-17 14:46:45 +0000

    commented JS

--- assets/script.js
+++ assets/script.js
@@ -1,5 +1,6 @@
+// Variable for the Moment.JS function, sets the format
 const date = moment().format("Do MMMM YYYY")
-console.log(date)
+// Variable to target the HTML element
 const dateHTML = document.getElementById("date")
-
+// Attaches the date to the HTML
 dateHTML.append(date)
